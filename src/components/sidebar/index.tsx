2772e487c542517e08144cbd4a3e1135476f5132
harshmangalam <harshdev8218@gmail.com>
2023-05-17 20:47:22 +0530

    added active routes

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,13 +1,28 @@
 import { component$ } from "@builder.io/qwik";
-import { HomeOutlineIcon, SearchOutlineIcon } from "~/icons";
+import {
+  HomeFilledIcon,
+  HomeOutlineIcon,
+  SearchFilledIcon,
+  SearchOutlineIcon,
+} from "~/icons";
 import { NavLinkItem } from "./nav-link-item";
 
 export const Sidebar = component$(() => {
   return (
     <aside class="col-span-3 row-span-5 h-full">
       <ul class="py-2 px-3">
-        <NavLinkItem icon={<HomeOutlineIcon />} name="Home" />
-        <NavLinkItem icon={<SearchOutlineIcon />} name="Search" />
+        <NavLinkItem
+          href="/"
+          icon={HomeOutlineIcon}
+          activeIcon={HomeFilledIcon}
+          name="Home"
+        />
+        <NavLinkItem
+          activeIcon={SearchFilledIcon}
+          href="/search/"
+          icon={SearchOutlineIcon}
+          name="Search"
+        />
       </ul>
     </aside>
   );
--- src/components/sidebar/nav-link-item.tsx
+++ src/components/sidebar/nav-link-item.tsx
@@ -1,16 +1,26 @@
 import { component$ } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
+import { Link, useLocation } from "@builder.io/qwik-city";
 
 interface NavLinkItemProps {
   name: string;
   icon: any;
+  href: string;
+  activeIcon: any;
 }
 export const NavLinkItem = component$((props: NavLinkItemProps) => {
-  const { name, icon } = props;
+  const { name, icon: Icon, href, activeIcon: ActiveIcon } = props;
+  const location = useLocation();
+
   return (
     <li class="py-1 px-3">
-      <Link href="/" class="flex gap-5 text-[#b3b3b3] hover:text-white h-10">
-        {icon}
+      <Link
+        href={href}
+        class={[
+          "flex gap-5 text-[#b3b3b3] hover:text-white h-10",
+          { "text-white": location.url.pathname === href },
+        ]}
+      >
+        {location.url.pathname === href ? <ActiveIcon /> : <Icon />}
         <span class="font-bold">{name}</span>
       </Link>
     </li>
