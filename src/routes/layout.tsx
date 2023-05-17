ceef5f0e7bea7ac5967ddad5e78c4855ab16a41b
harshmangalam <harshdev8218@gmail.com>
2023-05-17 20:03:26 +0530

    added root layout

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,13 @@
-import { component$, Slot } from '@builder.io/qwik';
+import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
-  return <Slot />;
+  return (
+    <div class="p-2 h-screen bg-black">
+      <div class="grid grid-cols-12 gap-2 grid-rows-6 h-full w-full">
+        <div class="bg-red-500 col-span-3 row-span-5"></div>
+        <div class="bg-green-500 col-span-9 row-span-5"></div>
+        <div class="bg-pink-500 col-span-12"></div>
+      </div>
+    </div>
+  );
 });
