bfa0b8f9a8faa692bcfe5666edaedaa26b95d5aa
harshmangalam <harshdev8218@gmail.com>
2023-05-17 20:26:48 +0530

    updated layout

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,11 +1,14 @@
-import { component$ } from "@builder.io/qwik";
+import { Slot, component$ } from "@builder.io/qwik";
+import { Sidebar } from "~/components/sidebar";
 
 export default component$(() => {
   return (
     <div class="p-2 h-screen bg-black">
       <div class="grid grid-cols-12 gap-2 grid-rows-6 h-full w-full">
-        <div class="bg-red-500 col-span-3 row-span-5"></div>
-        <div class="bg-green-500 col-span-9 row-span-5"></div>
+        <Sidebar />
+        <div class="bg-green-500 col-span-9 row-span-5">
+          <Slot />
+        </div>
         <div class="bg-pink-500 col-span-12"></div>
       </div>
     </div>
