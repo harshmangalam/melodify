19e4df0b6183afd77bbe42f4418906eb4806192c
harshmangalam <harshdev8218@gmail.com>
2023-05-18 19:01:06 +0530

    added home page top section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,13 +6,26 @@ export default component$(() => {
     <div>
       <div class="flex flex-col space-y-6 px-6">
         <ul class="grid grid-cols-2 gap-y-3 gap-x-6">
-          <LibraryItem />
-          <LibraryItem />
-          <LibraryItem />
-          <LibraryItem />
-          <LibraryItem />
-          <LibraryItem />
-          <LibraryItem />
+          <LibraryItem
+            avatarSrc="https://misc.scdn.co/liked-songs/liked-songs-640.png"
+            href="/collection/tracks"
+            title="Liked Songs"
+          />
+          <LibraryItem
+            avatarSrc="https://charts-images.scdn.co/assets/locale_en/regional/daily/region_in_default.jpg"
+            href="/collection/tracks"
+            title="Top 50 - India"
+          />
+          <LibraryItem
+            avatarSrc="https://charts-images.scdn.co/assets/locale_en/regional/daily/region_global_default.jpg"
+            href="/collection/tracks"
+            title="Top 50 - Global"
+          />
+          <LibraryItem
+            avatarSrc="https://seed-mix-image.spotifycdn.com/v6/img/romantic/2GoeZ0qOTt6kjsWW4eA6LS/en/default"
+            href="/collection/tracks"
+            title="Romantic Mix"
+          />
         </ul>
       </div>
     </div>
--- src/routes/library-item.tsx
+++ src/routes/library-item.tsx
@@ -1,21 +1,22 @@
 import { component$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
 
-export const LibraryItem = component$(() => {
+interface LibraryItemProps {
+  avatarSrc: string;
+  title: string;
+  href: string;
+}
+export const LibraryItem = component$((props: LibraryItemProps) => {
+  const { avatarSrc, href, title } = props;
   return (
     <li class="bg-[hsla(0,0%,100%,.1)] rounded h-16 transition duration-300 hover:bg-[hsla(0,0%,100%,.2)] flex items-center gap-4">
-      <img
-        width={64}
-        height={64}
-        src="https://misc.scdn.co/liked-songs/liked-songs-640.png"
-        class="rounded-l"
-      />
+      <img width={64} height={64} src={avatarSrc} class="rounded-l" />
 
       <Link
-        href="/"
+        href={href}
         class="flex-1 text-white text-ellipsis break-words font-bold "
       >
-        Liked Songs
+        {title}
       </Link>
     </li>
   );
