6b3a19a5b87b0fce647f4730538ddf86d306d40e
harshmangalam <harshdev8218@gmail.com>
2023-05-18 20:08:11 +0530

    added play pause button

---
+++ src/components/play-pause-button.tsx
@@ -0,0 +1,15 @@
+import { component$, useSignal } from "@builder.io/qwik";
+import { PauseIcon } from "~/icons/pause";
+import { PlayIcon } from "~/icons/play";
+
+export const PlayPauseButton = component$(() => {
+  const play = useSignal(false);
+  return (
+    <button
+      class="bg-green-base hover:bg-green-base-highlight rounded-full w-12 h-12 text-black grid place-items-center transition duration-300 hover:scale-105"
+      onClick$={() => (play.value = !play.value)}
+    >
+      {play.value ? <PauseIcon /> : <PlayIcon />}
+    </button>
+  );
+});
