1495b7f3702329bd6779dc5e6907abf8d9ed623d
harshmangalam <harshdev8218@gmail.com>
2023-05-18 19:29:20 +0530

    added pause icon

---
+++ src/icons/pause.tsx
@@ -0,0 +1,11 @@
+import { component$ } from "@builder.io/qwik";
+import { IconContainer } from "~/components/icon-container";
+
+export const PauseIcon = component$(() => (
+  <IconContainer>
+    <path
+      fill="currentColor"
+      d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"
+    ></path>
+  </IconContainer>
+));
