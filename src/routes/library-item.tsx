92d6c9eb9aa34871349866ed3b9c3cbfbce829cb
harshmangalam <harshdev8218@gmail.com>
2023-05-18 19:29:42 +0530

    added reusable library component

--- src/routes/library-item.tsx
+++ src/routes/library-item.tsx
@@ -1,5 +1,7 @@
-import { component$ } from "@builder.io/qwik";
+import { $, component$, useOn, useSignal } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
+import { PauseIcon } from "~/icons/pause";
+import { PlayIcon } from "~/icons/play";
 
 interface LibraryItemProps {
   avatarSrc: string;
@@ -8,9 +10,25 @@ interface LibraryItemProps {
 }
 export const LibraryItem = component$((props: LibraryItemProps) => {
   const { avatarSrc, href, title } = props;
+  const showAction = useSignal(false);
+  const play = useSignal(false);
+
+  useOn(
+    "mouseenter",
+    $(() => (showAction.value = true))
+  );
+  useOn(
+    "mouseleave",
+    $(() => (showAction.value = false))
+  );
   return (
     <li class="bg-[hsla(0,0%,100%,.1)] rounded h-16 transition duration-300 hover:bg-[hsla(0,0%,100%,.2)] flex items-center gap-4">
-      <img width={64} height={64} src={avatarSrc} class="rounded-l" />
+      <img
+        width={64}
+        height={64}
+        src={avatarSrc}
+        class="rounded-l w-16 h-16 "
+      />
 
       <Link
         href={href}
@@ -18,6 +36,15 @@ export const LibraryItem = component$((props: LibraryItemProps) => {
       >
         {title}
       </Link>
+
+      <div hidden={!showAction.value} class="px-4">
+        <button
+          class="bg-green-base hover:bg-green-base-highlight rounded-full w-12 h-12 text-black grid place-items-center transition duration-300 hover:scale-105"
+          onClick$={() => (play.value = !play.value)}
+        >
+          {play.value ? <PauseIcon /> : <PlayIcon />}
+        </button>
+      </div>
     </li>
   );
 });
