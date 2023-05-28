c434a9e383ef1496324b98202e370a4eddad3b95
harshmangalam <harshdev8218@gmail.com>
2023-05-28 11:42:05 +0530

    added forms plugin

--- src/routes/(auth)/signup/index.tsx
+++ src/routes/(auth)/signup/index.tsx
@@ -1,6 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 import { GoogleAuth } from "../google-auth";
 import { GithubAuth } from "../github-auth";
+import { Form } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
@@ -9,10 +10,18 @@ export default component$(() => {
         Sign up for free to start listening.
       </h2>
 
-      <div class="grid grid-cols-1 gap-2">
+      <section class="grid grid-cols-1 gap-2">
         <GoogleAuth />
         <GithubAuth />
-      </div>
+        <button class="flex space-x-6 border w-full border-essential-subdude rounded-full py-4 justify-center px-6 hover:border-white">
+          <span class="font-bold"> Continue with Phone Number</span>
+        </button>
+      </section>
+      <hr class="border border-essential-subdude my-4" />
+
+      <section>
+        <Form></Form>
+      </section>
     </div>
   );
 });
