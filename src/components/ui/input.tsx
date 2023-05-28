e0903baddef3eb3baa766c89a783c380c04a9013
harshmangalam <harshdev8218@gmail.com>
2023-05-28 12:02:19 +0530

    added input component

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,28 @@
+import { type QwikIntrinsicElements, component$ } from "@builder.io/qwik";
+import { ErrorIcon } from "~/icons";
+
+type HTMLInputProps = QwikIntrinsicElements["input"];
+type CustomProps = {
+  label?: string;
+  error?: string;
+};
+type InputProps = CustomProps & HTMLInputProps;
+export const Input = component$((props: InputProps) => {
+  const { label, error, ...inputProps } = props;
+  return (
+    <div class="flex flex-col space-y-2">
+      {label && (
+        <label for="email" class="text-sm font-bold">
+          {label}
+        </label>
+      )}
+      <input class="shadow-white rounded bg-base" {...inputProps} />
+      {error && (
+        <p class="flex items-center gap-2 text-negative">
+          <ErrorIcon />
+          <span class="text-sm ">{error}</span>
+        </p>
+      )}
+    </div>
+  );
+});
