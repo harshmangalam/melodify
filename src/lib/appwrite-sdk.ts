a43ea43eae792774733378ce7d3e8c01427d971f
harshmangalam <harshdev8218@gmail.com>
2023-05-30 20:40:47 +0530

    added signup action

--- src/lib/appwrite-sdk.ts
+++ src/lib/appwrite-sdk.ts
@@ -1,6 +1,14 @@
-import { Client } from "appwrite";
+import { Account, Client } from "appwrite";
 const endpoint=import.meta.env.VITE_APPWRITE_ENDPOINT
 const projectId=import.meta.env.VITE_APPWRITE_PROJECT_ID
-export const client = new Client()
+const client = new Client()
     .setEndpoint(endpoint)
     .setProject(projectId); 
+
+const account = new Account(client)
+
+export {
+    client,
+    account,
+   
+}
--- src/routes/(auth)/signup/index.tsx
+++ src/routes/(auth)/signup/index.tsx
@@ -1,11 +1,33 @@
 import { component$ } from "@builder.io/qwik";
 import { GoogleAuth } from "../google-auth";
 import { GithubAuth } from "../github-auth";
-import { Form, Link } from "@builder.io/qwik-city";
+import { Form, Link, routeAction$, z, zod$ } from "@builder.io/qwik-city";
 import { Input } from "~/components/ui/input";
 import { PhoneAuth } from "../phone-auth";
+import { account } from "~/lib/appwrite-sdk";
+import { AppwriteException, ID } from "appwrite";
 
+export const useSignup = routeAction$(
+  async ({ email, name, password }, { redirect, fail }) => {
+    try {
+      await account.create(ID.unique(), email, password, name);
+      redirect(303, "/login");
+    } catch (error) {
+      if (error instanceof AppwriteException) {
+        return fail(400, error.response as any);
+      }
+    }
+  },
+  zod$({
+    name: z.string().nonempty("Enter a name for your profile."),
+    email: z.string().email("You need to enter your email."),
+    password: z
+      .string()
+      .min(8, "Your password should be longer than 8 characters."),
+  })
+);
 export default component$(() => {
+  const action = useSignup();
   return (
     <div>
       <h2 class="font-bold text-2xl text-center mb-8">
@@ -20,13 +42,17 @@ export default component$(() => {
       <hr class="border border-essential-subdude my-4" />
 
       <section class="py-4">
-        <Form class="flex flex-col gap-4">
+        <div class="mb-4">
+          <p class="text-negative">{action.value?.message}</p>
+        </div>
+        <Form action={action} class="flex flex-col gap-4">
           <div class="flex flex-col space-y-2">
             <Input
               name="email"
               label="What's your email?"
               placeholder="Enter your email."
               id="email"
+              error={action.value?.fieldErrors?.email}
             />
             <Link
               href="/login?method=phone"
@@ -41,6 +67,7 @@ export default component$(() => {
             type="password"
             name="password"
             id="password"
+            error={action.value?.fieldErrors?.password}
           />
           <Input
             label="What should we call you?"
@@ -48,11 +75,13 @@ export default component$(() => {
             type="text"
             name="name"
             id="name"
+            error={action.value?.fieldErrors?.name}
           />
 
           <button
             type="submit"
-            class="font-bold text-lg rounded-full bg-green-base hover:bg-green-base-highlight px-4 py-3  text-black hover:scale-105"
+            class="font-bold text-lg rounded-full bg-green-base hover:bg-green-base-highlight px-4 py-3  text-black hover:scale-105 disabled:bg-bright-accent"
+            disabled={action.isRunning}
           >
             Sign up
           </button>
